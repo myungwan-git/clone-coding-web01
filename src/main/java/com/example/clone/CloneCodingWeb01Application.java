50911cfa4b06c18024c27e6199426b1f04eef765
myungwan-git <nsubway1070635@gmail.com>
2021-08-21 19:11:12 +0900

    20210821 clone coding first commit

---
+++ src/main/java/com/example/clone/CloneCodingWeb01Application.java
@@ -0,0 +1,13 @@
+package com.example.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCodingWeb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneCodingWeb01Application.class, args);
+	}
+
+}
